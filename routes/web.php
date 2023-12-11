e1f33f11e822ac068447e6bb28e985cd0e399e0b
Islomtursunboyev <153109759+Islomtursunboyev@users.noreply.github.com>
2023-12-11 12:14:00 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-
-        // Folder Routes
     Route::post('/folders', [FolderController::class, 'store'])->name('folders.store');
     Route::post('/subfolders', [FolderController::class, 'storeSubfolder'])->name('subfolders.store');
     Route::get('folders/{id}', [FolderController::class, 'show'])->name('folders.show');
-    // File Routes
-    // Route::get('/files/{id}', [FileController::class, 'show'])->name('files.show');
     Route::post('/files', [FileController::class, 'store'])->name('files.store');
     Route::delete('/files/{file}', [FileController::class, 'destroy'])->name('files.destroy');
 
@@ -36,10 +31,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/subfiles/{subfile}', [FileController::class, 'destroySubfile'])->name('subfiles.destroy');
 
     Route::get('shared/{encrypted_id}', [FileController::class, 'show'])->name('shared.link');
-    // Route::get('/files/{file}/share', [FileController::class,'share'])->name('files.share');
     Route::get('/shared/{encryptedId}', [FileController::class,'accessSharedFile'])->name('files.shared');
     Route::post('/share/{file}', [FileController::class, 'share'])->name('files.share');
-    // Route::post('share/{file}',[FileController::class,'share'])->name('share_file');
 
 });
 
