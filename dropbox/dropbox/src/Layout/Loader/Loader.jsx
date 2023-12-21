3f2e18e94d97f0365230b6c8357bbcf41720bba9
AbduazimAbduvaliyev <116269072+AbduazimAbduvaliyev@users.noreply.github.com>
2023-12-21 07:51:02 +0500

    dropbox

---
+++ dropbox/dropbox/src/Layout/Loader/Loader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Loader.scss'
+const Loader = () => {
+  return (
+<div class="loader">
+	<ul class="loader-boundry">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</div>
+  )
+}
+
+export default Loader
