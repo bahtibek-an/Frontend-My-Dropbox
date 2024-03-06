0ae365639b562d878f329896238f90c29e9ce096
User <abbostojiddinov0@gmail.com>
2024-03-06 06:06:57 +0500

    fix all

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./components/App"
-import "bootstrap/dist/css/bootstrap.min.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-    <App />,
-  document.getElementById("root")
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
